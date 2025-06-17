86fdcc9b85dd3ebddd4cca90ec95eed392010903
AlfredDaimari <alfred_daimari@yahoo.com>
2025-06-17 02:45:22 +0200

    fix: export

--- vagrant/remove.sh
+++ vagrant/remove.sh
@@ -1,12 +1,12 @@
 # set config using python library yq
 function set_config() {
-	MASTER_MEMORY=$(cat multipaper.toml | tomlq .master.memory)
-	MASTER_CPU=$(cat multipaper.toml | tomlq .master.cpu)
+	export MASTER_MEMORY=$(cat multipaper.toml | tomlq .master.memory)
+	export MASTER_CPU=$(cat multipaper.toml | tomlq .master.cpu)
 
-	WORKER_MEMORY=$(cat multipaper.toml | tomlq .worker.memory)
+	export WORKER_MEMORY=$(cat multipaper.toml | tomlq .worker.memory)
 
-  WORKER_TOTAL=$(cat multipaper.toml | tomlq .worker.total)
-  WORKER_CPU=$(cat multipaper.toml | tomlq .worker.cpu)
+  export WORKER_TOTAL=$(cat multipaper.toml | tomlq .worker.total)
+  export WORKER_CPU=$(cat multipaper.toml | tomlq .worker.cpu)
   #worker_cpus_str=$(cat multipaper.toml | tomlq .worker.cpu)
 	#worker_totals_str=$(cat multipaper.toml | tomlq .worker.total)
 	#worker_cpus_str=$(echo $worker_cpus_str | tr -d [ | tr -d ] | tr -d '\n' | tr -d ,)
@@ -14,9 +14,9 @@ function set_config() {
 	#IFS=',' read -r -a WORKER_CPUS <<<$worker_cpus_str
 	#IFS=',' read -r -a WORKER_TOTALS <<<$worker_totals_str
 
-	BOT_MEMORY=$(cat multipaper.toml | tomlq .bot.memory)
-	BOT_CPU=$(cat multipaper.toml | tomlq .bot.cpu)
-	BOT_TOTAL=$(cat multipaper.toml | tomlq .bot.total)
+	export BOT_MEMORY=$(cat multipaper.toml | tomlq .bot.memory)
+	export BOT_CPU=$(cat multipaper.toml | tomlq .bot.cpu)
+	export BOT_TOTAL=$(cat multipaper.toml | tomlq .bot.total)
 }
 
 set_config

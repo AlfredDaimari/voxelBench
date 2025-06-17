90a98947039fac318df5ef1b0bf0129098c16b35
AlfredDaimari <alfred_daimari@yahoo.com>
2025-06-17 23:28:29 +0200

    vagrant ssh fix

--- vagrant/vssh.sh
+++ vagrant/vssh.sh
@@ -1,3 +1,25 @@
+# set config using python library yq
+function set_config() {
+	export MASTER_MEMORY=$(cat multipaper.toml | tomlq .master.memory)
+	export MASTER_CPU=$(cat multipaper.toml | tomlq .master.cpu)
+
+	export WORKER_MEMORY=$(cat multipaper.toml | tomlq .worker.memory)
+
+  export WORKER_TOTAL=$(cat multipaper.toml | tomlq .worker.total)
+  export WORKER_CPU=$(cat multipaper.toml | tomlq .worker.cpu)
+  #worker_cpus_str=$(cat multipaper.toml | tomlq .worker.cpu)
+	#worker_totals_str=$(cat multipaper.toml | tomlq .worker.total)
+	#worker_cpus_str=$(echo $worker_cpus_str | tr -d [ | tr -d ] | tr -d '\n' | tr -d ,)
+	#worker_totals_str=$(echo $worker_totals_str | tr -d [ | tr -d ] | tr -d '\n' | tr -d ,)
+	#IFS=',' read -r -a WORKER_CPUS <<<$worker_cpus_str
+	#IFS=',' read -r -a WORKER_TOTALS <<<$worker_totals_str
+
+	export BOT_MEMORY=$(cat multipaper.toml | tomlq .bot.memory)
+	export BOT_CPU=$(cat multipaper.toml | tomlq .bot.cpu)
+	export BOT_TOTAL=$(cat multipaper.toml | tomlq .bot.total)
+}
+
+set_config
 if [[ $1 == "mult" ]]; then
   VAGRANT_VAGRANTFILE=MultVagrantfile.rb vagrant ssh $2
 else
